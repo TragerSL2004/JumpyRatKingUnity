3a34d6b599fd8da1fd32e0cf4394d31808bef554
Trager LeMaire <142815456+TragerSL2004@users.noreply.github.com>
2024-03-18 16:58:35 -0500

    Obstacles, platforms, and jumping added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField, Range(0, 100), Tooltip("Player max speed")] private float _maxSpeed;
-    [SerializeField, Range(0,100), Tooltip("Player Acceleration")] private float _acceleration;
+    [SerializeField, Tooltip("Player max speed")] private float _maxSpeed;
+    [SerializeField, Tooltip("Player Acceleration")] private float _acceleration;
+    [SerializeField, Tooltip("Player jump height")] private float _jumpHeight;
+
+    [Space]
+    [SerializeField] private Vector3 _groundCheck;
+    [SerializeField] private float _groundCheckRadius;
 
     private Rigidbody _rigidBody;
     private Vector3 _moveDirection;
+    private bool _jumpInput = false;
+    private bool _isGrounded = false;
 
     private void Awake()
     {
@@ -28,13 +35,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Get movement input
         _moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+        _jumpInput = Input.GetAxisRaw("Jump") != 0;
     }
     private void FixedUpdate()
     {
+        //Ground check
+        _isGrounded = Physics.OverlapSphere(transform.position + _groundCheck, _groundCheckRadius).Length > 1;
+        
+        //Add movement force
         _rigidBody.AddForce(_moveDirection * _acceleration * Time.deltaTime, ForceMode.VelocityChange);
 
-        if(_rigidBody.velocity.magnitude > _maxSpeed)
-            _rigidBody.velocity = _rigidBody.velocity.normalized * _maxSpeed;
+        //Clamp velocity to _maxSpeed
+        Vector3 velocity = _rigidBody.velocity;
+        float newXSpeed = Mathf.Clamp(_rigidBody.velocity.x, -_maxSpeed, _maxSpeed);
+        velocity.x = newXSpeed;
+        _rigidBody.velocity = velocity;
+
+        //Add jump force
+        if(_jumpInput && _isGrounded)
+        {
+            //Calculate force needed to reach _jumpHeight
+            float force = Mathf.Sqrt(_jumpHeight * -2f * Physics.gravity.y);
+            _rigidBody.AddForce(Vector3.up * force, ForceMode.Impulse);
+        }
     }
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        //Draw ground check
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position + _groundCheck, _groundCheckRadius);
+    }
+#endif
 }
